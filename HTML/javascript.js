5eb932f769f884677e181189a5304fc30172ed74
Y_RUKESH <yrukesh787@gmail.com>
2026-03-30 02:46:28 +0530

    Delete HTML directory

--- HTML/javascript.js
+++
@@ -1,8 +0,0 @@
-// Button click effect
-const buttons = document.querySelectorAll(".connect-buttons a");
-
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        alert("Opening link...");
-    });
-});
